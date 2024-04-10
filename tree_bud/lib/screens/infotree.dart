62cce7dae1f14860dd4b807fd1170e9bebef27c9
i511042 <i511042@student.fontys.nl>
2024-04-10 11:45:27 +0200

    added the image banner and appbar

--- tree_bud/lib/screens/infotree.dart
+++ tree_bud/lib/screens/infotree.dart
@@ -2,36 +2,53 @@ import 'package:ar_function/widgets/historyname.dart';
 import 'package:flutter/material.dart';
 
 class InfoTree extends StatelessWidget {
-  const InfoTree({Key? key}) : super(key: key);
+  const InfoTree({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Pamela'),
-        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(73, 97, 36, 1.0),
+        toolbarHeight: 40,
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Align(
-              alignment: AlignmentDirectional(0, 0),
-              child: Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 180, 0, 0),
-                child: Text(
-                  'History',
-                  style: TextStyle(
-                    fontFamily: 'Outfit',
-                    color: Color(0xFF15161E),
-                    fontSize: 24,
-                    fontWeight: FontWeight.w500,
-                  ),
+      backgroundColor: const Color.fromARGB(255, 118, 131, 109),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+           Image.asset(
+            'assets/images/boomstam.png',
+            height: 200, 
+            width: double.infinity, 
+            fit: BoxFit.cover,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 40.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+               const Padding(
+                padding: EdgeInsets.only(bottom: 14.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Pamela ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(
+                      Icons.info, 
+                      color: Colors.white, 
+                      size: 20),
+                    ]
                 ),
               ),
-            ),
-            Container(
+              
+              
+
+          Container(
             color: const Color.fromARGB(255, 10, 48, 11),
             child: const Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -50,6 +67,7 @@ class InfoTree extends StatelessWidget {
                     ),
                   ),
                 ),
+
                 ProfileInfo(
                   imageUrl: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
                   name: 'You',
@@ -57,12 +75,31 @@ class InfoTree extends StatelessWidget {
                   time: '5 hours ago',
                 ),
                 InfoCard(task: 'Watered tree ', time: '5 hours ago',),
+                
+                ProfileInfo(
+                  imageUrl: 'https://images.unsplash.com/photo-1558072844-b2e8b546d415?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D',
+                  name: 'Nicole Kerr', 
+                  task: 'Cleaned tree ', 
+                  time: '1 day ago'
+                ),
+                InfoCard(task: 'Cleaned tree', time: '1 day ago'),
+                
+                ProfileInfo(
+                  imageUrl: 'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fHByb2ZpbGUlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D',
+                  name: 'Simon Craig', 
+                  task: 'Fed tree ', 
+                  time: '3 days ago'
+                ),
+                InfoCard(task: 'Fed tree', time: '3 days ago'),
+
               ],
-    ),
-    ),
-    ],
-    ),
-    ),
-    );
+            ),
+          ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      );
   }
 }
